1ec807c055bbd13e1e4c75514556b71b7fd004a7
KhaledMoataz <32196766+KhaledMoataz@users.noreply.github.com>
2018-03-27 01:05:04 +0200

    Add files via upload

---
+++ Tower.h
@@ -0,0 +1,20 @@
+#pragma once
+class Tower
+{
+	double Health;
+	int Region;
+	//
+	// TODO: Add More Data Members As Needed
+	//
+
+public:
+
+	void SetHealth(double h);
+	double GetHealth() const;
+
+	//
+	// TODO: Add More Member Functions As Needed
+	//
+
+};
+
