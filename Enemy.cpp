541dc0b9378b780d67ae1549cd57a827b247ad56
KareemOsamaSobeih <37814454+KareemOsamaSobeih@users.noreply.github.com>
2018-03-30 22:57:55 +0200

    Enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,16 +1,22 @@
 #include "Enemy.h"
 
 
-Enemy::Enemy(color r_c, REGION r_region, int d)
+Enemy::Enemy()
 {
-	Clr = r_c;
-	Region = r_region;
-	SetDistance(d);
+	ArrivalTimeStamp = 0;
+	FirePower = 0;
+	ReloadPeriod = 0;
+	Health = 0;
+	Distance = MaxDistance;
+	FD = 0;
+	KD = 0;
+	LT = 0;
 }
 
 Enemy::~Enemy()
 {
 }
+
 
 color Enemy::GetColor() const
 {
@@ -40,3 +46,112 @@ int Enemy::GetDistance() const
 {
 	return Distance;
 }
+int Enemy::GetID()const
+{
+	return ID;
+}
+// Added funcs
+
+// set funcs
+
+void Enemy::SetID(int i)
+{
+	ID = i;
+}
+double Enemy::GetHealth()const
+{
+	return Health;
+}
+double Enemy::GetFirePower()const
+{
+	return FirePower;
+}
+int Enemy::GetArrivalTimeStamp()const
+{
+	return ArrivalTimeStamp;
+}
+int Enemy::GetReloadPeriod()const
+{
+	return ReloadPeriod;
+}
+int Enemy::GetFD()const
+{
+	return FD;
+}
+int Enemy::GetKD()const
+{
+	return KD;
+}
+int Enemy::GetLT()const
+{
+	return LT;
+}
+
+bool Enemy::IsReloadPeriod(int t)const
+{
+	return ((t - ArrivalTimeStamp) % ReloadPeriod != 0);
+}
+void Enemy::Attacked(double f, int t)
+{
+	if (!IsShot())
+		SetShot(t);
+	DecrementHealth(f);
+	if (Health <= 0)
+		SetKilled(t);
+}
+int Enemy::RemainingTimeforShot(int t)const
+{
+	if ((t - ArrivalTimeStamp) % ReloadPeriod != 0)
+		return (ReloadPeriod - (t - ArrivalTimeStamp) % ReloadPeriod);
+	else
+		return 0;
+}
+void Enemy::SetKilled(int t)
+{
+	Killed = true;
+	KD = t - FD;
+	LT = KD + FD;
+}
+bool Enemy::IsKilled()const
+{
+	return Killed;
+}
+bool Enemy::IsShot()const
+{
+	return FirstShot;
+}
+void Enemy::SetShot(int t)
+{
+	FirstShot = true;
+	FD = t - ArrivalTimeStamp;
+}
+
+
+
+void Enemy::SetHealth(double h)
+{
+	Health = (h > 0) ? h : 0;
+}
+void Enemy::SetFirePower(double f)
+{
+	FirePower = (f > 0) ? f : 0;
+}
+void Enemy::SetArrivalTimeStamp(int t)
+{
+	ArrivalTimeStamp = (t > 0) ? t : 0;
+}
+void Enemy::SetReloadPeriod(int r)
+{
+	ReloadPeriod = (r > 0) ? r : 0;
+}
+void Enemy::SetRegion(REGION R)
+{
+	Region = R;
+}
+void Enemy::SetColor()
+{}
+void Enemy::DecrementHealth(double f)
+{
+	Health -= f / Distance;
+}
+
