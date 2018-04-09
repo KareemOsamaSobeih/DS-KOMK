c741e3af487658d8e72568ca8b0acd3fa8752316
KareemOsamaSobeih <37814454+KareemOsamaSobeih@users.noreply.github.com>
2018-04-09 14:25:03 +0200

    Update Castle.cpp

--- Castle.cpp
+++ Castle.cpp
@@ -13,3 +13,19 @@ void Castle::SetTowersHealth(double h)
 		Towers[i].SetHealth(h);
 	}
 }
+void Castle::SetTowersFirePower(double f)
+{
+	for (int i = 0; i < NoOfRegions; i++)
+	{
+		Towers[i].SetFirePower(f);
+	}
+}
+bool Castle::CastleLose()const
+{
+	for (int i = 0; i < NoOfRegions; i++)
+	{
+		if(!Towers[i].IsKilled())
+			return false;
+	}
+	return true;
+}
